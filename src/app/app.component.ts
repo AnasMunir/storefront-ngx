ea6cff46c5847ae38b68707937e05fb6e3b31140
AnasMunir <anas.munir@bramerz.pk>
2018-10-24 23:41:36 +0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fishry-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'fishry-storefront-ngx';
+}
