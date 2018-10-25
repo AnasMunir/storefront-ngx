3b0ea16a3973cc684c706eef0893b4fb9c26239c
AnasMunir <anas.munir@bramerz.pk>
2018-10-25 18:12:57 +0500

    something amazing happend, trying to replicate again

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const vhost = require('vhost');
 
 let crimson = null;
+let classic = null;
 // const classic = require('./dist/classic/server');
 try {
   const crimsonApp = require('./dist/crimson/server');
@@ -9,16 +10,23 @@ try {
 } catch (error) {
   console.error('crimson dist error', error);
 }
+try {
+  const classicApp = require('./dist/classic/server');
+  classic = classicApp
+} catch (error) {
+  console.error('classic dist error', error);
+}
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-// app.use(vhost('classic2.stgfishry.com', classic));
 if (crimson) {
   app.use(vhost('crimson2.stgfishry.com', crimson));
 }
-
-let server = app.listen(PORT, () => {
+if (classic) {
+  app.use(vhost('classic2.stgfishry.com', classic));
+}
+app.listen(PORT, () => {
   console.log(`Node server listening on http://localhost:${PORT}`);
 });
 /* server.on('connection', (socket) => {
